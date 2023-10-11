8e93e74fbb578bc0eacb4542d32bc47f3c6b1ffd
Manisha Bagora <bagora.m@northeastern.edu>
2023-10-10 23:32:13 -0400

    my

---
+++ Lab10P1.java
@@ -0,0 +1,13 @@
+package javapractice;
+import java.util.Scanner;
+
+public class Lab10P1 {
+
+	public static void main(String[] args) {
+	String[] myWords= {"Dog","Cat","Fish","Bird","Horse"};
+	
+
+    	
+    }
+
+}
